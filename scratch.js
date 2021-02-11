267e0e62298db370e4d767e1ced99cc2d4b63b3e
nateoh93 <nathanoh93@gmail.com>
2021-02-10 21:34:25 -0800

    add practice problems

--- scratch.js
+++ scratch.js
@@ -779,3 +779,26 @@ var twoCitySchedCost = function(costs) {
 
 // console.log(twoCitySchedCost([[10,20],[30,200],[400,50],[30,20]]))
 // console.log(twoCitySchedCost([[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]))
+
+function isPalindrome(input) {
+    const alpha = new Set('abcdefghijklmnopqrstuvwxyz0123456789'.split(''));
+    let reversedStr = '';
+    let str = '';
+
+    for (let i = 0; i < input.length; i++) {
+        let currChar = input[i].toLowerCase();
+        if (!alpha.has(currChar)) continue;
+        reversedStr = currChar + reversedStr;
+        str += currChar;
+    };
+    console.log(str, reversedStr);
+    return str === reversedStr;
+}
+
+// console.log(isPalindrome("A man, a plan, a canal: Panama"))
+// console.log(isPalindrome("0P"))
+
+[["1","1","1","1","0"],
+ ["1","1","0","1","0"],
+ ["1","1","0","0","0"],
+ ["0","0","0","0","0"]]
